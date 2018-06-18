48978e90c1d3149aa0c029fa19a13bb05d9c9ec3
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-18 16:22:45 +0100

    get film by id route

--- controllers/films.js
+++ controllers/films.js
@@ -5,6 +5,10 @@ const Review = require('../models/review');
 const express = require('express');
 const filmRouter = new express.Router();
 
+filmRouter.get('/:id', function(req, res){
+  const index = req.params.id;
+  res.json({data: films[index]})
+});
 
 filmRouter.get('/', function(req, res){
   res.json(films);
