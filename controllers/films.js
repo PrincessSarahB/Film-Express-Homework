fd456b44f90ab0928e75014096545e834f0cc684
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-18 16:20:32 +0100

    view all films route

--- controllers/films.js
+++ controllers/films.js
@@ -1,3 +1,14 @@
 const films = require('../models/films')();
 const Film = require('../models/film');
 const Review = require('../models/review');
+
+const express = require('express');
+const filmRouter = new express.Router();
+
+
+filmRouter.get('/', function(req, res){
+  res.json(films);
+});
+
+
+module.exports = filmRouter;
